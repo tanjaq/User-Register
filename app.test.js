51836cd7487db1250b8d8b41ec7237c8e3abc6a2
Setletint <72040591+Setletint@users.noreply.github.com>
2024-02-19 10:45:49 +0200

    Made test cases for app.js
    app.test.js tests coverage - 100%
    all tests coverage - 100%
    
    app.test.js run time - 0.912s/0.712s
    all tests run time - 0.922s/0.734s

--- app.test.js
+++ app.test.js
@@ -5,6 +5,38 @@ const validatePassword = require('./validation/validatePassword')
 
 const app = createApp(validateUsername, validatePassword)
 
+describe('General request tests', ()=>{
+    test('request method shoul be a POST', async ()=>{
+        const response = await request(app).post('/users').send({
+            username: 'Username',
+            password: 'Password123'
+        })
+        expect(response.req.method).toBe('POST');
+    })
+    test('request should have a right entered values', async ()=>{
+        const response = await request(app).post('/users').send({
+            username: 'User',
+            password: 'badpass'
+        })
+        expect(response.request._data.username).toBe('User')
+        expect(response.request._data.password).toBe('badpass')  
+    })
+    test('request should be application/json type', async ()=>{
+        const response = await request(app).post('/users').send({
+            username: 'Username',
+            password: 'Password123'
+        })
+        expect(response.type).toBe('application/json')
+    })
+    test('no username and no password given', async ()=>{
+        const response = await request(app).post('/users').send({
+            username: '',
+            password: ''
+        })
+        expect(response.statusCode).toBe(400)
+    })
+})
+
 describe('given correct username and password', () => {
     test('return status 200', async () => {
         const response = await request(app).post('/users').send({
@@ -13,4 +45,64 @@ describe('given correct username and password', () => {
         })
         expect(response.statusCode).toBe(200)
     })
+    test('result have userId', async ()=>{
+        const response = await request(app).post('/users').send({
+            username: 'Username',
+            password: 'Password123'
+        })
+        expect(response.body.userId).toBeDefined()
+    })
+    test('result have positive message', async ()=>{
+        const response = await request(app).post('/users').send({
+            username: 'Username',
+            password: 'Password123'
+        })
+        expect(response.body.message).toBe("Valid User");
+    })
+})
+
+describe('given incorrect username or Password', ()=>{
+    test('return status 400', async ()=>{
+        const response = await request(app).post('/users').send({
+            username: 'User',
+            password: 'badpass'
+        })
+        expect(response.statusCode).toBe(400);
+    })
+    test('return an error in body', async ()=>{
+        const response = await request(app).post('/users').send({
+            username: 'User',
+            password: 'badpass'
+        })
+        expect(response.body.error).toBe("Invalid User");
+    })
+    test('result should not have userId', async ()=>{
+        const response = await request(app).post('/users').send({
+            username: 'User',
+            password: 'badpass'
+        })
+        expect(response.body.userId).not.toBeDefined()
+    })
+    test('no username was given', async ()=>{
+        const response = await request(app).post('/users').send({
+            username: '',
+            password: 'badpass'
+        })
+        expect(response.statusCode).toBe(400);
+    })
+    test('only username is correct', async ()=>{
+        const response = await request(app).post('/users').send({
+            username: 'AgoodUsername',
+            password: 'badpass'
+        })
+        expect(response.statusCode).toBe(400);
+    })
+    test('only password is correct', async ()=>{
+        const response = await request(app).post('/users').send({
+            username: 'User',
+            password: 'AgoodPassword123'
+        })
+        expect(response.statusCode).toBe(400);
+    })
+
 })
