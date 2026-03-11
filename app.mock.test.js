932317ad82ec1796306cbea07880c6da23b55f58
Tauri <Tauritammela272@gmail.com>
2026-03-11 09:00:56 +0200

    Ülesanne 2: Mocked API testid

---
+++ app.mock.test.js
@@ -0,0 +1,46 @@
+jest.mock('./validation/validateEmail')
+
+const createApp = require('./app')
+const request = require('supertest')
+const validateUsername = require('./validation/validateUsername')
+const validatePassword = require('./validation/validatePassword')
+const validateEmail = require('./validation/validateEmail')
+
+const app = createApp(validateUsername, validatePassword, validateEmail)
+
+describe('given correct username and password', () => {
+    test('return status 200', async () => {
+        validateEmail.mockReturnValue(true)
+
+        const response = await request(app).post('/users').send({
+            username: 'Username',
+            password: 'Password123',
+            email: 'student@example.com'
+        })
+        expect(response.statusCode).toBe(200)
+    })
+
+    test('returns userId', async () => {
+        validateEmail.mockReturnValue(true)
+
+        const response = await request(app).post('/users').send({
+            username: 'Username',
+            password: 'Password123',
+            email: 'student@example.com'
+        })
+        expect(response.body.userId).toBeDefined();
+    })
+})
+
+describe('given incorrect or missing username and password', () => {
+    test('return status 400', async () => {
+        validateEmail.mockReturnValue(false)
+
+        const response = await request(app).post('/users').send({
+            username: 'user',
+            password: 'password',
+            email: 'not-an-email'
+        })
+        expect(response.statusCode).toBe(400)
+    })
+})
